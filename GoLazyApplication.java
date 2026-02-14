95add34004810ee50b129b9a368b0026fa090aaa
TechmastersTrainings <Techmasterstrainings@gmail.com>
2026-02-14 23:14:53 +0530

    Add files via upload

---
+++ GoLazyApplication.java
@@ -0,0 +1,13 @@
+package com.techmasters.GoLazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoLazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoLazyApplication.class, args);
+	}
+
+}
